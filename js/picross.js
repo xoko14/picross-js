81a4c61079fdd9524f8a6f4077402f3373ffd360
Xoko14 <xoquinperezb@gmail.com>
2020-04-24 12:49:04 +0200

    read picross board input

--- js/picross.js
+++ js/picross.js
@@ -1,3 +1,6 @@
+var piRAM; //global variable for storing current picross string
+
+//show picross board with input data
 function crearTabla(){
     var tabla = document.getElementById("tabla");
     var largo = document.getElementById("largo").value;
@@ -20,10 +23,16 @@ function crearTabla(){
     }
 }
 
+//generate picross string from picross board
 function generate(){
     var largo = document.getElementById("largo").value;
     var alto = document.getElementById("alto").value;
-    var code = document.getElementById("code");
+    if(largo.length == 1){
+        largo = "0"+largo;
+    }
+    if(alto.length == 1){
+        alto = "0"+alto;
+    }
     var picross = [largo,alto];
     for(i=1;i<=largo*alto;i++){
         if(document.getElementById(i).checked == true){
@@ -33,6 +42,60 @@ function generate(){
             picross.push(0);
         }
     }
-    console.log(picross);
-    code.innerHTML = picross;
+    piRAM = picross.toString().replace(/,/g, '');
+    console.log(piRAM);
+}
+
+//get an specific row
+function getRow(tRow){
+    var picrossImp = piRAM;
+    var equis = picrossImp[0]+picrossImp[1];
+    var row = [];
+    for(i=0;i<equis;i++){
+        row.push(picrossImp[4+tRow*equis+i]);
+    }
+    return row;
+}
+
+function getCol(tCol){
+    var picrossImp = piRAM;
+    var iy = picrossImp[2]+picrossImp[3];
+    var col = [];
+    for(i=0;i<iy;i++){
+        col.push(picrossImp[4+tCol+iy*i]);
+    }
+    return col;
+}
+
+function getStringInfo(tType,tStr){
+    switch(tType){
+        case "row":
+            var Str = getRow(tStr);
+            break;
+        case "col":
+            var Str = getCol(tStr);
+            break;
+        default:
+            console.log("Error, input not nice.");
+            break;
+    }
+    var conde = 0;
+    var final = [];
+    console.log(Str.length + ", " + Str);
+    for(i=0;i<Str.length;i++){
+        if(Str[i]==1){
+            conde++;
+        }
+        else{
+            final.push(conde);
+            conde = 0;
+        }
+    }
+    final.push(conde);
+    for(i=final.length-1;i>=0;i--) {
+        if(final[i] === 0) {
+            final.splice(i, 1);
+        }
+    }
+    return final.toString().replace(/,/g, ' ');
 }
